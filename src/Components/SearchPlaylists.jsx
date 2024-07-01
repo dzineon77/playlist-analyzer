5ebd6e4968d4947567903ff12a23a5c378098e3e
Dzineon G <dzineon77@gmail.com>
2024-06-30 22:43:00 -0400

    api calls work + authorization works for others

--- src/Components/SearchPlaylists.jsx
+++ src/Components/SearchPlaylists.jsx
@@ -1,12 +1,19 @@
-
-export default function SearchPlaylists() {
+export default function SearchPlaylists({ playlists }) {
     return (
         <div className='SearchPlaylists'>
             <div className="header">
                 <h1>Search Playlists</h1>
                 <p>Choose from one of your own playlists</p>
             </div>
-
+            <ul>
+                {playlists.map((playlist, index) => (
+                    <li key={index}>
+                        <p>Name: {playlist.name}</p>
+                        <p>URI: {playlist.uri}</p>
+                        <p>Tracks Total: {playlist.tracks.total}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
--- src/Pages/Analyze.jsx
+++ src/Pages/Analyze.jsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { getToken } from './Home.jsx';
 import SearchPlaylists from '../Components/SearchPlaylists.jsx';
 import SearchUsers from '../Components/SearchUsers.jsx';
-
+import { getToken } from './Home.jsx';
 
 export default function Analyze() {
-
     const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'));
+    const [isToggled, setIsToggled] = useState(false);
+    const [playlists, setPlaylists] = useState([]);
 
     useEffect(() => {
         getToken(setAccessToken);
+        getPlaylists();
     }, []);
-
-    const [isToggled, setIsToggled] = useState(false);
 
     const handleToggleChange = () => {
         setIsToggled(!isToggled);
+    };
+
+    const getPlaylists = async () => {
+        try {
+            const response = await fetch('https://api.spotify.com/v1/me/playlists?limit=100', {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            });
+            const data = await response.json();
+            setPlaylists(data.items);
+            console.log('Playlists:', data.items)
+        } catch (error) {
+            console.error('Error fetching playlists:', error);
+        }
     };
 
     return (
@@ -27,22 +41,24 @@ export default function Analyze() {
             </div>
 
             <div className="container">
-
                 <div className="search">
-                    <input type="checkbox" id="toggle" class="toggleCheckbox" onChange={handleToggleChange} // Update to handle change
-                        checked={isToggled}/>
-                        <label htmlFor="toggle" class='toggleContainer'>
-                            <div>Search Users</div>   
-                            <div>Your Playlists</div>
-                        </label>
+                    <input
+                        type="checkbox"
+                        id="toggle"
+                        className="toggleCheckbox"
+                        onChange={handleToggleChange}
+                        checked={isToggled}
+                    />
+                    <label htmlFor="toggle" className="toggleContainer">
+                        <div>Search Users</div>
+                        <div>Your Playlists</div>
+                    </label>
                 </div>
 
                 <div className="results">
-                {isToggled ? <SearchPlaylists /> : <SearchUsers />}
-
+                    {isToggled ? <SearchPlaylists playlists={playlists}/> : <SearchUsers />}
                 </div>
             </div>
-
         </div>
     );
 }
